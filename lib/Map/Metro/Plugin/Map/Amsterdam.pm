d395089a63e1552de46b374dfa24456892c9c32e
Erik Carlsson <info@code301.com>
2015-01-25 18:46:34 +0100

    Doc cleanup

--- lib/Map/Metro/Plugin/Map/Amsterdam.pm
+++ lib/Map/Metro/Plugin/Map/Amsterdam.pm
@@ -32,33 +32,21 @@ Map::Metro::Plugin::Map::Amsterdam - Map::Metro map for Amsterdam
     use Map::Metro;
     my $graph = Map::Metro->new('Amsterdam')->parse;
 
+Or:
+
+    $ map-metro.pl route Amsterdam Isolatorweg Gein
+
 =head1 DESCRIPTION
 
 See L<Map::Metro> for usage information.
 
 =head1 Status
 
-As of 2015-01-01 it contains lines 50, 51, 53 and 54. See (L<wikipedia|https://en.wikipedia.org/wiki/Amsterdam_metro>).
+This map L<contains|Map::Metro::Plugin::Map::Amsterdam::Lines>:
 
-See L<Map::Metro::Plugin::Map::Amsterdam::Lines>.
+=for :list
+* Lines 50, 51, 53 and 54 [L<wikipedia|https://en.wikipedia.org/wiki/Amsterdam_metro>]
 
-=begin HTML
-
-<p><a href="https://raw.githubusercontent.com/Csson/p5-Map-Metro-Amsterdam/master/static/images/amsterdam.png"><img src="https://raw.githubusercontent.com/Csson/p5-Map-Metro-Amsterdam/master/static/images/amsterdam.png" style="max-width: 600px" /></a></p>
-
-=end HTML
-
-=head1 AUTHOR
-
-Erik Carlsson E<lt>info@code301.comE<gt>
-
-=head1 COPYRIGHT
-
-Copyright 2015 - Erik Carlsson
-
-=head1 LICENSE
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+=for HTML <p><a href="https://raw.githubusercontent.com/Csson/p5-Map-Metro-Amsterdam/master/static/images/amsterdam.png"><img src="https://raw.githubusercontent.com/Csson/p5-Map-Metro-Amsterdam/master/static/images/amsterdam.png" style="max-width: 600px" /></a></p>
 
 =cut
